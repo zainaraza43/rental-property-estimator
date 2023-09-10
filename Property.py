965ede74fb7df296d068560768beb55ec69bb46d
zainaraza43 <urboyzain@gmail.com>
2023-09-09 20:20:39 -0400

    refactoring omg!

---
+++ Property.py
@@ -0,0 +1,32 @@
+import re
+from enum import Enum
+
+
+class Property:
+    class PropertyType(Enum):
+        SINGLE_FAMILY = "Single Family"
+        VACANT_LAND = "Vacant Land"
+
+    def __init__(self, property_data: dict):
+        # MLS Fields
+        self.property_id = int(property_data['Id'])
+        self.mls_number = property_data['MlsNumber']
+        self.description = property_data['PublicRemarks']
+
+        # Location Fields
+        match = re.match(r'(.+)\|(.+), (.+) (.+)', property_data['Property']['Address']['AddressText'])
+        self.street_address = match.group(1).strip()
+        self.city = match.group(2).strip()
+        self.province = match.group(3).strip()
+        self.postal_code = match.group(4).strip()
+        self.latitude = float(property_data['Property']['Address']['Latitude'])
+        self.longitude = float(property_data['Property']['Address']['Longitude'])
+
+        # Property Attributes Fields
+        self.property_type = property_data['Property']['Type']
+        self.price = int(property_data['Property']['PriceUnformattedValue'])
+
+        self.bedrooms = sum(map(int, property_data['Building']['Bedrooms'].replace('+',
+                                                                                   ' ').split())) if self.property_type == Property.PropertyType.SINGLE_FAMILY else 0
+        self.bathrooms = sum(map(int, property_data['Building']['BathroomTotal'].replace('+',
+                                                                                         ' ').split())) if self.property_type == Property.PropertyType.SINGLE_FAMILY else 0
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
-from dotenv import load_dotenv
+
 import requests
+from dotenv import load_dotenv
 from ratelimit import limits, sleep_and_retry
+
+from Property import Property
 
 load_dotenv()
 
@@ -18,48 +21,47 @@ I = 100
 WINDSOR_PROPERTY_TAX = 0.01853760
 
 
-@sleep_and_retry
-@limits(calls=3, period=ONE_MINUTE)
-def call_api(url):
+class PropertyAnalyzer:
+    def __init__(self):
+        self.url = "https://realty-in-ca1.p.rapidapi.com/properties/list-residential"
+        self.headers = {
+            "X-RapidAPI-Key": os.environ.get("RAPIDAPI_KEY"),
+            "X-RapidAPI-Host": os.environ.get("RAPIDAPI_HOST")
+        }
 
-    querystring = {"LatitudeMax": "42.309842", "LatitudeMin": "42.298578", "LongitudeMax": "-83.045481", "LongitudeMin": "-83.095084",
-                   "CurrentPage": "1", "RecordsPerPage": "10", "SortOrder": "A", "SortBy": "1", "CultureId": "1", "NumberOfDays": "0", "BedRange": "0-0", "BathRange": "0-0", "RentMin": "0"}
+    @sleep_and_retry
+    @limits(calls=3, period=ONE_MINUTE)
+    def call_api(self):
+        querystring = {
+            "LatitudeMax": "42.309842",
+            "LatitudeMin": "42.298578",
+            "LongitudeMax": "-83.045481",
+            "LongitudeMin": "-83.095084",
+            "CurrentPage": "1",
+            "RecordsPerPage": "10",
+            "SortOrder": "A",
+            "SortBy": "1",
+            "CultureId": "1",
+            "NumberOfDays": "0",
+            "BedRange": "0-0",
+            "BathRange": "0-0",
+            "RentMin": "0"
+        }
 
-    headers = {
-        "X-RapidAPI-Key": os.environ.get("RAPIDAPI_KEY"),
-        "X-RapidAPI-Host": os.environ.get("RAPIDAPI_HOST")
-    }
-    response = requests.request(
-        "GET", url, headers=headers, params=querystring)
+        response = requests.get(self.url, headers=self.headers, params=querystring)
 
-    if response.status_code != 200:
-        raise Exception('API response: {}'.format(response.status_code))
-    return response
+        if response.status_code != 200:
+            raise Exception('API response: {}'.format(response.status_code))
+
+        return response
+
+    def analyze_properties(self):
+        response_dict = self.call_api().json()
+        for property_data in response_dict['Results']:
+            property = Property(property_data)
+            print(property.street_address)
 
 
-url = "https://realty-in-ca1.p.rapidapi.com/properties/list-residential"
-
-for i in range(1):
-    response_dict = call_api(url).json()
-    for property in response_dict['Results']:
-        try:
-            if property['Building']['Type'] == "House":
-                P = int(property['Property']['PriceUnformattedValue']) * (1 - DOWN_PAYMENT)
-                M = (P * R * (1 + R) ** N) / (((1 + R) ** N) - 1)
-                T = WINDSOR_PROPERTY_TAX * int(property['Property']['PriceUnformattedValue']) / 12
-                beds = 0
-                for c in property['Building']['Bedrooms']:
-                    if c.isdigit():
-                        beds += int(c)
-                PM = 0.1 * AVERAGE_ROOM_PRICE * beds
-                total_cost = M + T + I + PM + MISC
-
-                print(f"{property['Property']['Address']['AddressText']}")
-                print(f"{property['Building']['StoriesTotal']} story {property['Building']['Type']} | {property['Building']['Bedrooms']} bed(s), {property['Building']['BathroomTotal']} bath(s)")
-                print(f"{property['Property']['Type']} {property['Property']['Price']}")
-                # print(f"NOTES: {property['PublicRemarks']}")
-                print(f"{M:.2f} (Mortgage) + {T:.2f} (Taxes) + {I} (Insurance) + {PM} (Property Management) + {MISC} (MISC) = ${total_cost:.2f}")
-                print(f"Assuming average room price of {AVERAGE_ROOM_PRICE}, total revenue/month = ${AVERAGE_ROOM_PRICE * beds}")
-                print(f"Profit = {AVERAGE_ROOM_PRICE * beds - total_cost:.2f}, down payment of ${int(property['Property']['PriceUnformattedValue']) * DOWN_PAYMENT:.2f}\n")
-        except KeyError:
-            print(f"=================================================================\nignoring {property['Property']['Address']['AddressText']}\n=================================================================")
+if __name__ == "__main__":
+    analyzer = PropertyAnalyzer()
+    analyzer.analyze_properties()
